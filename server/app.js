34646fbae9883cbd685818adf13024d59d657f7f
MomentaryChen <zzser15963@gmail.com>
2020-02-12 14:08:25 +0800

    Eslint ignore check server

--- server/app.js
+++ server/app.js
@@ -1,30 +1,34 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+let app = express();
 
 app.use('/public', express.static('public'));
 
-app.get('/', function (req, res) {
-    res.redirect('/index.html');
+app.get('/', (req, res) => {
+  res.redirect('/index.html');
 });
 
-app.get('/index.html', function (req, res) {
-    res.sendFile(__dirname + "/" + "index.html");
+app.get('/index.html', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/process_get', function (req, res) {
+app.get('/test', (req, res) => {
+  res.send('TEST');
+});
 
-    // 输出 JSON 格式
-    var response = {
-        "first_name": req.query.first_name,
-        "last_name": req.query.last_name
-    };
-    console.log(response);
-    res.end(JSON.stringify(response));
+app.get('/process_get', (req, res) => {
+
+  // 输出 JSON 格式
+  let response = {
+    "first_name": req.query.first_name,
+    "last_name": req.query.last_name
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
 });
 
 let server = app.listen(8000, function () {
-    let host = server.address().address;
-    let port = server.address().port;
+  let host = server.address().address;
+  let port = server.address().port;
 
-    console.log('Example app listening on http://%s:%s', host, port);
+  console.log(`Example app listening on http://${host}:${port}`);
 });
